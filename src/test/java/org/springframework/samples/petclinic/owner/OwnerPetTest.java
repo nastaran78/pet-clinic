a44a0f90adac7c2b4b1a476daa41848b09b5d600
Nastaran Alipour <nastaran.alipour@divar.ir>
2021-10-15 19:35:42 +0330

    merge Part2

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerPetTest.java
@@ -0,0 +1,69 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.BeforeClass;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(Theories.class)
+public class OwnerPetTest {
+
+	public static Pet pet;
+	public static Owner owner;
+	@DataPoints
+	public static String[] data = {"RABBIT", "Cat", "dog"};
+	@DataPoints
+	public static Set[] datasets = {new HashSet(Arrays.asList("cat", "dog")),
+		new HashSet(Arrays.asList("cat", "rabbit", "dog")),
+		new HashSet(Arrays.asList("fish", "rabbit", "dog", "bird"))};
+
+	@BeforeClass
+	public static void setUp() {
+		owner = new Owner();
+	}
+
+	@Theory
+	public void petNameExists(Set<String> dataset, String name) throws IllegalAccessException {
+
+		assumeNotNull(dataset);
+		assumeTrue(dataset.contains(name) || dataset.contains(name.toLowerCase()));
+		setPets(dataset);
+		pet = owner.getPet(name, false);
+
+		assertNotNull(pet);
+	}
+
+	@Theory
+	public void petNameNotExists(Set<String> dataset, String name) throws IllegalAccessException {
+
+		assumeNotNull(dataset);
+		assumeTrue(!dataset.contains(name) && !dataset.contains(name.toLowerCase()));
+		setPets(dataset);
+		pet = owner.getPet(name, false);
+		assertNull(pet);
+	}
+
+	private void setPets(Set<String> dataset) throws IllegalAccessException {
+		List<String> datalist = new ArrayList<>(dataset);
+		List<Pet> pets = new ArrayList<>();
+
+		for (String s : datalist) {
+			Pet pet = new Pet();
+			pet.setName(s);
+			pets.add(pet);
+		}
+		Field petsField = GetObjectField.getPrivateFiled(owner, "pets");
+		assertNotNull(petsField);
+		petsField.set(owner, new HashSet<>(pets));
+	}
+
+}
--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -1,11 +1,53 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.BeforeClass;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.samples.petclinic.visit.Visit;
 
+
 import java.time.LocalDate;
+import java.util.*;
 
-class PetTest {
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
-	// todo
+@RunWith(Theories.class)
+public class PetTest {
+
+	public static Pet pet = new Pet();
+
+	@BeforeClass
+	public static void setUp(){
+		pet = new Pet();
+	}
+
+	@DataPoints
+	public static Set[] datasets = { new HashSet(Arrays.asList (LocalDate.parse("2019-01-12") , LocalDate.parse("2017-01-12"))),
+									new HashSet(Arrays.asList (LocalDate.parse("2019-01-12") , LocalDate.parse("2019-02-12"))),
+									new HashSet(Arrays.asList (LocalDate.parse("2019-01-12") , LocalDate.parse("2019-02-18") , LocalDate.parse("2019-02-15")))};
+
+	@Theory
+	public void isSortingVisitsDateCorrectly(Set<LocalDate> dates){
+		assumeTrue(dates!=null);
+		List<LocalDate> dateslist = new ArrayList<>(dates);
+		List<Visit> visits = new ArrayList<>();
+
+		for(int i=0 ; i<dateslist.size() ; i++){
+			Visit visit = new Visit();
+			visit.setDate(dateslist.get(i));
+			visits.add(visit);
+		}
+		pet.setVisitsInternal(visits);
+		List<Visit> sortedvisit = pet.getVisits();
+		Collections.sort(dateslist , Collections.reverseOrder());
+
+		for(int j=0; j<visits.size() ; j++){
+			assertTrue("Sorting visits is not correct",sortedvisit.get(j).getDate().equals(dateslist.get(j)));
+		}
+	}
+
 }
